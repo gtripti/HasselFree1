d103638626ba4d60fde0bee60ad7cf71954039f4
gtripti <tripti.gupta97@gmail.com>
2019-04-26 14:29:54 +0530

    Initial commit

---
+++ app/src/main/java/scom/example/aashshar/hasselfree/Invoice.java
@@ -0,0 +1,13 @@
+package scom.example.aashshar.hasselfree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Invoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice);
+    }
+}
